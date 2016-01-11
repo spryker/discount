91bc60b54cbe14018114f137a734f66e3bb33bf4
Auris <aurimas.lickus@spryker.com>
2016-01-11 17:15:23 +0100

    875: removed checkout connectors

---
+++ src/Spryker/Zed/Discount/Business/Model/DiscountSaverInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+namespace Spryker\Zed\Discount\Business\Model;
+
+use Generated\Shared\Transfer\CheckoutResponseTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+
+interface DiscountSaverInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     * @param \Generated\Shared\Transfer\CheckoutResponseTransfer $checkoutResponseTransfer
+     */
+    public function saveDiscounts(QuoteTransfer $quoteTransfer, CheckoutResponseTransfer $checkoutResponseTransfer);
+
+}
