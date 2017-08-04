70c013ebafbeb002bfc80bdccb42f032173aea36
René Klatt <rene.klatt@spryker.com>
2017-08-04 05:54:53 +0000

    core-1652 fixed some tests

--- tests/SprykerTest/Zed/Discount/_support/DiscountCommunicationTester.php
+++ tests/SprykerTest/Zed/Discount/_support/DiscountCommunicationTester.php
@@ -1,10 +1,4 @@
 <?php
-
-/**
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
- */
-
 namespace SprykerTest\Zed\Discount;
 
 use Codeception\Actor;
@@ -24,10 +18,10 @@ use Codeception\Actor;
  *
  * @SuppressWarnings(PHPMD)
  */
-class CommunicationTester extends Actor
+class DiscountCommunicationTester extends Actor
 {
 
-    use _generated\CommunicationTesterActions;
+    use _generated\DiscountCommunicationTesterActions;
 
    /**
     * Define custom actions here
